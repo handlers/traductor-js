15bd622b5a8fbea383cfb74cf4d3416038c35a95
Sam Handler <sam.handler@gmail.com>
2015-01-25 14:30:05 -0500

    Add header and clarifications

--- traductor.js
+++ traductor.js
@@ -17,9 +17,10 @@ function translateText(text) {
   })
 }
 
-function makeTranslationContainer() {
+function makeTranslationContainer(text) {
   translation_container = '<div class="_traductor">' +
     '<div class="_traductor_quit_container"><span class="_traductor_quit">x</span></div>' +
+    '<div class="_traductor_header">' + text + "</div>" +
     '<div class="_traductor_body">' +
     '<button class="_traductor_translate">Get Definition</button>' +
     '</div><div class="_traductor_clear"></div></div>'
@@ -47,16 +48,30 @@ function buildListItemMarkup(obj) {
   list_item_markup = '<li class="_traductor_definition">' +
     '<span class="_traductor_part_of_speech">' + 
     definition.partofspeech + '</span>: ' +
+    buildClarificationsMarkup(definition.clarifications) +
     '<span class="_traductor_definition">' + 
     definition.text + '</span>' +
     '</li>'
-  return list_item_markup 
+  return list_item_markup;
+}
+
+function buildClarificationsMarkup(clarifications) {
+  clarifications_markup = ""
+  if (clarifications !== undefined) {
+    $.each(clarifications, function(i, c) {
+      clarifications_markup += '<span class="_traductor_clarification">('
+      clarifications_markup += clarifications[i]
+      clarifications_markup +=')</span> '
+    });
+  }
+  return clarifications_markup;
 }
 
 function parseDefinitionObject(obj) {
   definition = {};
   definition.partofspeech = obj.partofspeech.partofspeechdisplay;
   definition.text = obj.text;
+  definition.clarifications = obj.clarification
   return definition;
 }
 
@@ -67,7 +82,7 @@ function traductorIsShowing() {
 $(document).on("mouseup", function(e){
   text = getSelectedText(); 
   if (text && !traductorIsShowing()) {
-    translation_container = makeTranslationContainer();
+    translation_container = makeTranslationContainer(text);
     positionTranslationContainer(e.pageX, e.pageY);
   }
 });
@@ -76,7 +91,6 @@ $(document).on('click', '._traductor_translate', function(e) {
   translateText(text).done(function(result) {
     translation_markup = buildTranslationMarkup(result);
     $("._traductor_body").html(translation_markup);
-    //addQuitHandler();
   });
 })
 
